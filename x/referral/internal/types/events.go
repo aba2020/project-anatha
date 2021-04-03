29eecc9a8c2de0b88804533ed1796ded4e56685d
Andrej Budinčević <andrej@block-blox.com>
2021-04-03 20:55:59 +0200

    Referral module events

---
+++ x/referral/internal/types/events.go
@@ -0,0 +1,16 @@
+package types
+
+const (
+	EventTypeWithdrawReferralRewards         = "withdraw_referral_rewards"
+	EventTypeCharityFundDistributionProposal = "CharityFundDistributionProposal"
+
+	AttributeKeyCaller				= "caller"
+	AttributeKeyAddress  			= "address"
+	AttributeKeySender              = "sender"
+	AttributeKeyTitle               = "title"
+	AttributeKeyDescription         = "description"
+	AttributeKeyAmount              = "amount"
+	AttributeKeyRecipient           = "recipient"
+
+	AttributeValueModule = ModuleName
+)
