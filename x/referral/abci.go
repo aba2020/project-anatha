54a6a417ef624af9a8fa8f0dc1b0b42a13173ccd
Andrej Budinčević <andrej@block-blox.com>
2021-04-03 20:58:31 +0200

    ABCI logic implementation

---
+++ x/referral/abci.go
@@ -0,0 +1,12 @@
+package referral
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	abci "github.com/tendermint/tendermint/abci/types"
+)
+
+func BeginBlocker(ctx sdk.Context, req abci.RequestBeginBlock, k Keeper) {
+}
+
+func EndBlocker(ctx sdk.Context, k Keeper) {
+}
